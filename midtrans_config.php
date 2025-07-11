33a07ed76ef9a9d0e7a0b8011a003b36fac03d2d
Salsabilla <salsabilla05student@gmail.com>
2025-07-11 23:25:12 +0700

    Update midtrans_config.php

--- midtrans_config.php
+++ midtrans_config.php
@@ -1,7 +1,7 @@
 <?php
 require_once 'vendor/autoload.php'; // path ke autoload composer
 
-\Midtrans\Config::$serverKey = 'SB-Mid-server-eFkUEIBGj-d_5rthSD-3WP_T';
+\Midtrans\Config::$serverKey = 'SBxxxx';
 \Midtrans\Config::$isProduction = false; // Ganti ke true saat live
 \Midtrans\Config::$isSanitized = true;
 \Midtrans\Config::$is3ds = true;
